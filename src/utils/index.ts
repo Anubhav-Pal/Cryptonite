bc158ca9bc66aa7cc921d5e44657443a310ab4a8
Anubhav Pal <anubhavpal.excl@gmail.com>
2024-07-31 22:19:48 +0530

    fix: cors error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ export const apiKey = process.env.NEXT_PUBLIC_API_KEY || "";
 export const apiOptions = {
   method: "GET",
   headers: {
+    "Access-Control-Allow-Origin": "*",
     accept: "application/json",
     "x-cg-demo-api-key": apiKey,
     mode: "no-cors",
